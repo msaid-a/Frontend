0fa5d41a0a9869680db6fbe34026e48811fbc105
Muhammad Said Arrafi <muhammadsaidarrafi@gmail.com>
2019-09-23 11:08:33 +0700

    term on App

--- day10/second-react/src/component/App.jsx
+++ day10/second-react/src/component/App.jsx
@@ -2,12 +2,17 @@ import React, {Component} from 'react';
 import Search from './search'
 
 class App extends Component{
+
+  onSearchSubmit = (term) =>{
+    console.log(term)
+  }
+
   render (){
     return (
       <div className="container">
       <header>
         <h1 className ="text-center mt-3 mb-5">Image Search Engine</h1>
-          <Search />
+          <Search onSearchSubmit = {this.onSearchSubmit}/>
       </header>
     </div>
   );
--- day10/second-react/src/component/search.jsx
+++ day10/second-react/src/component/search.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
 class search extends Component {
+
+    // tempat menyimpan data component
+    state = {
+        term: ''
+    }
+
+    onSubmitFrom = (event) =>{
+        // fungction yang akan mengcancel refresh
+        event.preventDefault()
+        // cara akses propoerti di luar class (file yang berbeda)
+        this.props.onSearchSubmit(this.state.term)
+    }
+
     render() {
         return (
             <div>
-               <form className="form-group" onSubmit={(event) =>{event.preventDefault()}}>
-                   <input type="text" className="form-control" onChange={(event) => {console.log(event.target.value)}}/>
+               <form className="form-group" onSubmit={this.onSubmitFrom}>
+                   <input 
+                        type="text" 
+                        className="form-control" 
+                        onChange={(event) => {this.setState({term: event.target.value})}}/>
                 </form> 
             </div>
         )
@@ -13,8 +29,10 @@ class search extends Component {
 }
 
 export default search
-
+// event.target.value berisi string yang di input oleh user
 // Even Headler bertugas menghendle suatu event terjadu 
     // event : onClick, onSubmit, onChange, ...
 // onChange memukinkan kita untuk memanggil function ketika ada perubahan di text input 
 // onSubmit memukinkan kita untuk memanggil function ketika di klik enter 
+
+// setiap komponen memilliki tempat penyimpanan data yang namanya adalah state
