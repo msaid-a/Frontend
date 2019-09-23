0515a45af700bc7332f93b3160652b20a9aa3d9c
Muhammad Said Arrafi <muhammadsaidarrafi@gmail.com>
2019-09-23 09:27:35 +0700

    search bar react

---
+++ day10/second-react/src/component/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Search from './search'
+
+function App() {
+  return (
+    <div className="container">
+      <header>
+        <h1 className ="text-center mt-3 mb-5">Image Search Engine</h1>
+        <Search />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ day10/second-react/src/component/search.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class search extends Component {
+    render() {
+        return (
+            <div>
+               <form action="form-group">
+                   <input type="text" className="form-control" name="" id=""/>
+                </form> 
+            </div>
+        )
+    }
+}
+
+export default search
