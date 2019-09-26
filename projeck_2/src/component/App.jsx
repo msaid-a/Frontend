e29cd15f52a8c8eb967b7317d5d93a77fd1025ab
Muhammad Said Arrafi <muhammadsaidarrafi@gmail.com>
2019-09-26 14:17:50 +0700

    fakeApi Json + ref atribute

--- projeck_2/src/component/App.jsx
+++ projeck_2/src/component/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 
+
 import Login from './Login'
 import Home from './Home'
 import Register from './Register'
--- projeck_2/src/component/Register.jsx
+++ projeck_2/src/component/Register.jsx
@@ -1,13 +1,47 @@
 import React, { Component } from 'react'
 
  class Register extends Component {
+
+    onSubmitClick = () =>{
+        //  ambil data dari text boz
+        let username = this.username.value
+        let email = this.email.value
+        let password = this.sandi.value
+        // simpan data di json
+
+        console.log(username, email, password)
+    }
+
     render() {
         return (
-            <div>
-                <h1>Register Component</h1>
+            <div className=" col-5 mx-auto mt-5 card">
+                <div className='card-body'>
+                    <div className='border-bottom border-secondary card-tittle'>
+                        <h1>Register</h1>
+                    </div>
+                    <form className="form-group">
+                        <div className='card-title'>
+                            <h4>Username</h4>
+                        </div>
+                        <input className="form-control" type="text" name="username" id="" ref={(input) => {this.username =  input}} />
+                        <div className='card-title'>
+                            <h4>Email</h4>
+                        </div>
+                        <input className="form-control" type='email'ref={(input) => {this.email =  input}} />
+                        <div className='card-title'>
+                            <h4>Password</h4>
+                        </div>
+                        <input className="form-control" type="password" name="password" id=""ref={(input) => {this.sandi =  input}} />
+                    </form>
+                    <button onClick={this.onSubmitClick} className="btn btn-outline-danger btn-block" >Register</button>
+                </div>
             </div>
         )
     }
 }
 
 export default Register
+
+// Untuk memberukan referensu ke suatu inputan, kita harus menggunakan atribut 'ref'
+    // akan meneruma function, function yang di terima menerima 1 inputan 
+    // nama inputa itu bebas, namun biasanya akan di beri nama input
